472ab1ba92956692434efe23f1134ec2ae42d7cc
Josh Lindsey <joshua.s.lindsey@gmail.com>
2015-04-17 11:24:34 -0400

    Fix specs

--- spec/bay_photo_spec.rb
+++ spec/bay_photo_spec.rb
@@ -2,11 +2,5 @@ require "spec_helper"
 
 RSpec.describe BayPhoto do
   it { should be_a(Module) }
-
-  %i[Configuration].each do |mod|
-    it "autoloads #{mod}" do
-      expect(subject.autoload? mod).not_to be_nil
-    end
-  end
 end
 

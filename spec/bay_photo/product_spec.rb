745a26eee8211138cbe839ca059d458ce7ab5c8d
Josh Lindsey <joshua.s.lindsey@gmail.com>
2015-04-23 15:31:42 -0400

    Flesh out Product specs

--- spec/bay_photo/product_spec.rb
+++ spec/bay_photo/product_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
 
 RSpec.describe BayPhoto::Product do
+  EXPECTED_ATTRIBUTES = %i[id name price print_size_x print_size_y square_inch]
+  EXPECTED_ATTRIBUTES.each do |attr|
+    it { should respond_to(attr) }
+    it { should respond_to(:"#{attr}=") }
+  end
+
+  it "should define the correct resource" do
+    expect(subject.resource).to eq(:products)
+  end
 end
 
